7fef0cefc83427ee343d006f7a27121b017fbd0c
IvannaHrytsiuk <izink0.0@gmail.com>
2020-09-25 17:18:13 +0300

    add routes

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { UsersComponent } from './components/users/users.component';
+import { AccountComponent } from './components/account/account.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path:'login', component: LoginComponent},
+  {path:'dashboard', component: MainPageComponent },
+  {path: 'users', component: UsersComponent},
+  {path: 'user/:id', component: UserDetailsComponent },
+  {path: 'my-page', component: AccountComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
