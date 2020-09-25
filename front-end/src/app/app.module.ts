68f28d468c76d67ee167b20e4c5fdad5b1dd69b7
IvannaHrytsiuk <izink0.0@gmail.com>
2020-09-25 17:09:13 +0300

    add components

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { UsersComponent } from './components/users/users.component';
+import { AccountComponent } from './components/account/account.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainPageComponent,
+    UsersComponent,
+    AccountComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
