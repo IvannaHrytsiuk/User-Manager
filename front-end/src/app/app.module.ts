f49f7f894eeb245dd590ab93e37e42d0711258dd
IvannaHrytsiuk <izink0.0@gmail.com>
2020-09-28 19:04:09 +0300

    create header-footer

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,9 +4,13 @@ import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
