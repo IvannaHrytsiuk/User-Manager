33024238d12ff635ec67f0c0167d50f0a97bcfcf
IvannaHrytsiuk <izink0.0@gmail.com>
2020-09-28 19:19:53 +0300

    add main-page route and view

--- front-end/src/app/core/core-routing.module.ts
+++ front-end/src/app/core/core-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from '../login/login/login.component';
 
+import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo:'login', pathMatch:'full'},
   {path: 'login', loadChildren: () => import('../login/login.module').then(m=> m.LoginModule)},
+  {path: 'dashboard', component: MainPageComponent}
   ];
   
 
