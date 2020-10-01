69cdf55077ceb5129309e4978406942e2a162020
IvannaHrytsiuk <izink0.0@gmail.com>
2020-10-01 10:19:48 +0300

    add crypto password and jwt auth

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql');
 const express = require('express');
-const crypto = require('crypto');
 
 let app = express();
 
 const bodyParser = require('body-parser');
+
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+const jwt = require('jsonwebtoken');
 
 app.use(bodyParser.json());
 
@@ -71,13 +74,73 @@ app.delete('/users/:id', (req, res)=>{
 // });
 app.post('/users', (req, res) => {
     let user = req.body;
-    var sql = "INSERT INTO `users`(name,email, password, created_at, updated_at) VALUES (?,?, ?, NOW(), NOW());";
-    mysqlConnection.query(sql, [user.name, user.email, user.password], (err, rows, fields) => {
-        if (!err){            
-            res.send('New ');
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).send({
+            msg: err
+          });
+        } else {
+            let sql = `INSERT INTO users (name,email, password, created_at, updated_at) VALUES (?,?, ${mysqlConnection.escape(hash)}, NOW(), NOW());`;
+            mysqlConnection.query(sql, [user.name, user.email, user.password], (err, rows, fields) => {
+                if (!err){            
+                     res.send({status:'ok'});
+                } else{
+                    console.log(err);
+                }
+            });
         }
-        else{
-            console.log(err);
+      });
+    
+});
+
+app.post('/login', (req, res) => {
+    mysqlConnection.query(
+        `SELECT * FROM users WHERE email = ${mysqlConnection.escape(req.body.email)};`,
+        (err, result) => {
+          if (err) {
+            throw err;
+            return res.status(400).send({
+              msg: err
+            });
+          }
+    
+          if (!result.length) {
+            return res.status(401).send({
+              msg: 'Username or password is incorrect!'
+            });
+          }
+    
+          bcrypt.compare(
+            req.body.password,
+            result[0]['password'],
+            (bErr, bResult) => {
+              if (bErr) {
+                throw bErr;
+                return res.status(401).send({
+                  msg: 'Username or password is incorrect!'
+                });
+              }
+    
+              if (bResult) {
+                const token = jwt.sign({
+                    email: result[0].email,
+                    id: result[0].id
+                  },
+                  'secretsecretkey', {
+                    expiresIn: '7d'
+                  }
+                );
+                return res.status(200).send({
+                  msg: 'Logged in!',
+                  token,
+                  user: result[0]
+                });
+              }
+              return res.status(401).send({
+                msg: 'Username or password is incorrect!'
+              });
+            }
+          );
         }
-    })
+      );
 });
