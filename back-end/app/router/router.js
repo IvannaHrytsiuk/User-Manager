04949f88279d7800f2dc8b794328c0db832eb981
IvannaHrytsiuk <izink0.0@gmail.com>
2020-10-02 13:01:47 +0300

    add JWT Authentication & Authorization

---
+++ back-end/app/router/router.js
@@ -0,0 +1,15 @@
+const verifySignUp = require('./verifySignUp');
+const authJwt = require('./verifyJwtToken');
+ 
+module.exports = function(app) {
+ 
+    const controller = require('../controller/controller.js');
+ 
+  app.post('/api/auth/signup', [verifySignUp.checkDuplicateUserNameOrEmail, verifySignUp.checkRolesExisted], controller.signup);
+  
+  app.post('/api/auth/signin', controller.signin);
+  
+  app.get('/api/test/user', [authJwt.verifyToken], controller.userContent);
+    
+  app.get('/api/test/admin', [authJwt.verifyToken], controller.adminBoard);
+}
---
+++ back-end/server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json())
+ 
+require('./app/router/router.js')(app);
+ 
+const db = require('./app/config/db.config.js');
+ 
+const Role = db.role;
+  
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync with { force: true }');
+  initial();
+});
+ 
+ 
+var server = app.listen(8080, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("App listening at http://%s:%s", host, port)
+})
+ 
+ 
+function initial(){
+  Role.create({
+    id: 1,
+    name: "USER"
+  });
+  
+  Role.create({
+    id: 2,
+    name: "ADMIN"
+  });
+}
