7943310ea94382d87a059e9da975cc3e0c2a2881
IvannaHrytsiuk <izink0.0@gmail.com>
2020-10-06 08:58:16 +0300

    add new db

--- back-end/Dump20201006.sql
+++ back-end/Dump20201006.sql
@@ -66,7 +66,7 @@ CREATE TABLE `user_roles` (
 
 LOCK TABLES `user_roles` WRITE;
 /*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
-INSERT INTO `user_roles` VALUES ('2020-10-02 12:48:27','2020-10-02 12:48:27',1,2),('2020-10-02 12:52:19','2020-10-02 12:52:19',1,3),('2020-10-02 12:52:39','2020-10-02 12:52:39',1,4),('2020-10-05 07:00:24','2020-10-05 07:00:24',1,5),('2020-10-02 12:47:52','2020-10-02 12:47:52',2,1);
+INSERT INTO `user_roles` VALUES ('2020-10-02 12:48:27','2020-10-02 12:48:27',1,2),('2020-10-02 12:52:19','2020-10-02 12:52:19',1,3),('2020-10-06 05:55:00','2020-10-06 05:55:00',1,13),('2020-10-06 05:55:34','2020-10-06 05:55:34',1,14),('2020-10-02 12:47:52','2020-10-02 12:47:52',2,1);
 /*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,7 +86,7 @@ CREATE TABLE `users` (
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +95,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','admin','admin@gmail.com','$2a$08$.LbE2oex7f9M6Oj7kuVvEefy1rGwO1YYQlFLW7dumXYVlvLkEiDkW','2020-10-02 12:47:52','2020-10-02 12:47:52'),(2,'ivan','ivanov','ii@gmail.com','$2a$08$/uFbBBlGHx311w6yX77ose6.G8eAJ0Vgvx4JIYEYnDj/.7Pg6vvIK','2020-10-02 12:48:27','2020-10-02 12:48:27'),(3,'petro','petro','pp@gmail.com','$2a$08$vmCoLi4rs8kURHKnphvae.KuvpIp3gnJdClQ47DUBCIRdRSylXQ/S','2020-10-02 12:52:19','2020-10-02 12:52:19'),(4,'iryna','iryna','ira@gmail.com','$2a$08$ry2nHaw4vDRuHRiLwkx34usmYJDUQctEVE/L0IjM0UPNxdUjpSn9u','2020-10-02 12:52:39','2020-10-02 12:52:39'),(5,'oleh','oleh','oo@gmail.com','$2a$08$L2KRrCwN6Ykj4WLqNrsDFuZDr/CzVtLijaKrNLW5Weiaq0jyCqCji','2020-10-05 07:00:24','2020-10-05 07:00:24');
+INSERT INTO `users` VALUES (1,'admin','admin','admin@gmail.com','$2a$08$.LbE2oex7f9M6Oj7kuVvEefy1rGwO1YYQlFLW7dumXYVlvLkEiDkW','2020-10-02 12:47:52','2020-10-02 12:47:52'),(2,'Ivan','Ivanov','ii@gmail.com','$2a$08$/uFbBBlGHx311w6yX77ose6.G8eAJ0Vgvx4JIYEYnDj/.7Pg6vvIK','2020-10-02 12:48:27','2020-10-05 17:23:26'),(3,'Petro','petro','pp@gmail.com','$2a$08$vmCoLi4rs8kURHKnphvae.KuvpIp3gnJdClQ47DUBCIRdRSylXQ/S','2020-10-02 12:52:19','2020-10-05 17:50:45'),(13,'iryna','ira','ira@gmail.com','$2a$08$IXbVtV.XRDcFfwPRQzKdpOEG2fa9o28HEV/Fi1YO/1kxxqVD.C3MG','2020-10-06 05:55:00','2020-10-06 05:55:00'),(14,'sai','sai','sai@gmail.com','$2a$08$4rMPT0Y3lJ4YKzrfdT9pju6fVyIkhoTCztXcQx62APeHVq6SUpD.K','2020-10-06 05:55:34','2020-10-06 05:55:34');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -108,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-05 12:46:03
+-- Dump completed on 2020-10-06  8:56:28
