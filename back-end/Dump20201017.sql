543dd7840729badd7d13ff881e90f21bd14adc60
IvannaHrytsiuk <izink0.0@gmail.com>
2020-10-17 08:20:36 +0300

    add new db

--- back-end/Dump20201017.sql
+++ back-end/Dump20201017.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `testdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `testdb`;
 -- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: testdb
@@ -109,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-15 14:55:55
+-- Dump completed on 2020-10-17  8:15:38
